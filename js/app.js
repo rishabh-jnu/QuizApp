d4ebe4d85154eedd80d0399cad8db62ede737913
Your Name <rishabhyadav70070@gmail.com>
2024-04-20 13:35:24 +0530

    adding array of object in app.js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const questions = [{
+    'que': 'Which of the following is a markup Language?',
+    'a': 'HTML',
+    'b': 'CSS',
+    'c': 'JavaScript',
+    'd': 'PHP',
+    'correct': 'a'
+},
+{
+    'que': 'In which year JavaScritp launched?',
+    'a': '1996',
+    'b': '1995',
+    'c': '1994',
+    'd': 'None of the above',
+    'correct': 'b'
+},
+{
+    'que': 'What does CSS stands for?',
+    'a': 'Hypertext Markup Language',
+    'b': 'Cascading Style Sheet',
+    'c': 'Jason Object Notation',
+    'd': 'Cascading Sheet Style',
+    'correct': 'b'
+}
+]
